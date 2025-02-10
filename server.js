b3eb683cfa862ffa57de01272af915de0b7f0734
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-02-10 14:06:14 +0900

    서버 연동

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // https://expressjs.com/
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = 3000;
 
+app.use(cors()); // 미들웨어 모두에게 오픈
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.send("Hello World!");
+  res.send("Bye Earth!");
 });
 
 app.listen(port, () => {
